0d1ef2501df5e440b073d3d3144c4231c4678390
bo wang <wangbostc@hotmail.com>
2026-03-08 16:29:13 +1100

    Add initial project files including configuration, styles, and main application structure

---
+++ frontend/src/main.tsx
@@ -0,0 +1,34 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './index.css'
+
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
+import DashboardPage from './pages/DashboardPage'
+import ReadPage from './pages/ReadPage'
+import ShopPage from './pages/ShopPage'
+import LessonsPage from './pages/LessonsPage'
+import ProgressPage from './pages/ProgressPage'
+import RewardsPage from './pages/RewardsPage'
+import AdminPage from './pages/AdminPage'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/read" element={<ReadPage />} />
+        <Route path="/shop" element={<ShopPage />} />
+        <Route path="/lessons" element={<LessonsPage />} />
+        <Route path="/progress" element={<ProgressPage />} />
+        <Route path="/rewards" element={<RewardsPage />} />
+        <Route path="/admin" element={<AdminPage />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>,
+)
